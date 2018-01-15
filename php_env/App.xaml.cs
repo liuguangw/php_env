a341742e525941de5754e11ea8e30f72d9268f36
liuguang <67579722@qq.com>
2018-01-15 11:20:10 +0800

    处理可能未捕获的异常

--- php_env/App.xaml.cs
+++ php_env/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace php_env
 {
@@ -13,5 +7,15 @@ namespace php_env
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// 处理未捕获的异常
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            MessageBox.Show(e.Exception.Message, "出错了", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
 }
